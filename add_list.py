039df475380536f481acb3f8beddcf4dd5af5e79
Vova Kudrin <kudrin19i93@gmail.com>
2024-12-08 17:35:15 +0700

    Update add_list.py

--- add_list.py
+++ add_list.py
@@ -1,8 +1,8 @@
 title1 = input('Введите заголовок №1: ')
 title2 = input('Введите заголовок №2: ')
 title3 = input('Введите загловок №3: ')
-cooking = [title1, title2, title3]
+list_of_headers = [title1, title2, title3]
 print(title1)
 print(title2)
 print(title3)
-print(cooking)
+print(list_of_headers)
