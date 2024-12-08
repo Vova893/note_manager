fbf4ac865c107e15c8e9ae8437ccdaded4c1bf86
Vova Kudrin <kudrin19i93@gmail.com>
2024-12-08 15:24:12 +0700

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,7 @@
+title1 = input('Лучшие рецепты ')
+title2 = input('Рецепты первых блюд ')
+title3 = input('Рецепты вторых блюд ')
+cooking = [title1, title2, title3]
+print(title1)
+print(title2)
+print(title3)
