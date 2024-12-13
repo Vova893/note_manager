8b3720e139de34420c3b53632c9f6b15b0442456
Vova Kudrin <kudrin19i93@gmail.com>
2024-12-13 18:19:20 +0700

    add_list.py

--- add_list.py
+++ add_list.py
@@ -1,8 +1,8 @@
 title1 = input('Введите заголовок №1: ')
 title2 = input('Введите заголовок №2: ')
 title3 = input('Введите загловок №3: ')
-list_of_headers = [title1, title2, title3]
+list_title = [title1, title2, title3]
 print(title1)
 print(title2)
 print(title3)
-print(list_of_headers)
+print(list_title)
